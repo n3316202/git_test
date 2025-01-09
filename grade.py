46023cdadc282d71c4a231c52115c557a1360638
tom <admin@admin.com>
2025-01-09 17:17:51 +0900

    성정표 완성성

--- grade.py
+++ grade.py
@@ -1,9 +1,15 @@
+from calendar import c
+
+
 class Grade:
     def __init__(self,kor,eng,math):
         self.kor = kor
         self.eng = eng
-        self.math = math
-    
+        self.math = math    
+
+    def set_eng(self,eng):
+        self.eng = eng
+
     def get_total(self):
         return self.kor + self.eng + self.math
 
@@ -28,18 +34,28 @@ class Grade:
 
         return grade
 
-
+# 
 def main():
-    kor = int(input("국어 점수 입력 : "))
-    eng = int(input("영어 점수 입력 : "))
-    math = int(input("수학 점수 입력 : "))
 
-    grade = Grade(kor,eng,math)
+    while True:
+        kor = int(input("국어 점수 입력 : "))
+        eng = int(input("영어 점수 입력 : "))
+        math = int(input("수학 점수 입력 : "))
 
-    print("총점 : ",grade.get_total())
-    print("평균 : ",grade.get_avg())
-    print("학점 : ",grade.get_grade())
+        grade = Grade(kor,eng,math)        
+        
+        print("국어 : ",grade.kor)
+
+        print("총점 : ",grade.get_total())
+        print("평균 : ",grade.get_avg())
+        print("학점 : ",grade.get_grade())
+
+        continue_yn = input("계속 하시겠습니까? (y/n) : ")
+        #y,yes,Yes,Y,yes
+        if continue_yn.upper() != 'Y' and continue_yn.upper() != 'YES' :
+            break
     
+    print("프로그램 종료")
 
 main()
 
