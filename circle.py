5027d50ef7967e644ae33cbf9dfbe9d58350ec6f
tom <admin@admin.com>
2025-01-13 16:59:27 +0900

    commit

--- circle.py
+++ circle.py
@@ -7,3 +7,14 @@ def ci_circle(r):
     return 2 * PI * r # 원둘계 계산
 
 
+a = 1000
+b = 1000
+print(a == b) #True
+print(id(a),id(b))
+print(a is b)
+
+a = 3
+b = 3
+print(a == b)
+print(id(a),id(b))
+print(a is b)
